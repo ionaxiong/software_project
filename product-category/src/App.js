f9e238ff2dc8fc02464b54a6fec350fbf2e4e28d
Ming Xiong <iona.m.xiong@gmail.com>
2021-09-06 21:20:41 +0300

    manipulate data

--- product-category/src/App.js
+++ product-category/src/App.js
@@ -4,16 +4,50 @@ import * as api from "./api";
 function App() {
   useEffect(() => {
     async function fetchData() {
+      // get product from api
       const products = await api.getProducts();
-      console.log(products)
+      console.log(products);
 
-      // filter by category
+      //print each product
+      products.forEach((p) => console.log(p));
+
+      //get category from api
+      const categories = await api.getCategory();
+      console.log(categories);
+
+      //print each category
+      categories.forEach((c) => console.log(c));
+
+      //select id from category where title = "meat"
+      //filter by category "meat"
+      const meatCategoryId = categories.find(
+        (category) => category.title === "meat"
+      ).id;
+      console.log(meatCategoryId);
+
+      const meatProducts = products.filter(
+        (product) => product.category === meatCategoryId
+      );
+      console.log(meatProducts);
 
       // map to title
+      const productTitles = products.map((product) => product.title);
+      console.log(productTitles);
 
-      // get by id 
+      //sort products
+      console.log({ ...products.sort() });
+
+      //sort products by price
+      console.log({ ...products.sort((a, b) => a.price - b.price) });
+
+      //sort products by stocks
+      console.log({ ...products.sort((a, b) => a.pcs - b.pcs) });
+
+      // get by id
+      const product = products.find((p) => p.id === "3");
+      console.log(product);
     }
-    fetchData()
+    fetchData();
   }, []);
 
   return <div></div>;
